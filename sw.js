0489adec462d9e20805607a20150a88093a28692
djalmaremondini-ship-it <djalmaremondini@gmail.com>
2026-06-14 19:26:32 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - Base para Push Notifications futuras
    ============================================================ */
 
-const CACHE_NAME   = 'rotera-v2';
+const CACHE_NAME   = 'rotera-v4';
 const CACHE_STATIC = [
   '/campos-jordao-guia/',
   '/campos-jordao-guia/index.html',
